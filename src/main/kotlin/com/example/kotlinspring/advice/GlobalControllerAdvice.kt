6f7bcd3bcc3486ff65f02af92ffd695a277021ed
bsh6463 <bsh9510@gmail.com>
2022-04-25 22:31:50 +0900

    @ControllerAdvice

---
+++ src/main/kotlin/com/example/kotlinspring/advice/GlobalControllerAdvice.kt
@@ -0,0 +1,21 @@
+package com.example.kotlinspring.advice
+
+import com.example.kotlinspring.controller.exception.ExceptionApiController
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice(basePackageClasses = [ExceptionApiController::class]) //RestController에 대한 Advice
+class GlobalControllerAdvice {
+
+    @ExceptionHandler(value = [RuntimeException::class])
+    fun exception(e: RuntimeException): String {
+        return "Server Error"
+    }
+
+    @ExceptionHandler(value = [IndexOutOfBoundsException::class])
+    fun indexOutOfBoundsException(e: IndexOutOfBoundsException): ResponseEntity<String>? {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Index Error")
+    }
+}
